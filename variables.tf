748dbdcd048a7085bd34c1475ff9c40c2c7654da
ömer yücel <omeryucel02@gmail.com>
2024-07-31 21:36:50 +0300

    terraform-my-phonebook-project

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "xxxxxxxxxxxxxx"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "xxxxxxxxxxxxxxx"
+}
+
+variable "hosted-zone" {
+  default = "xxxxxxxxxxxxxxxx"
+}
